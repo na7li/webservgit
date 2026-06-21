e2ee2547fa7bff363d017d596db4ef86e4579d85
mnahli <mohamed.nahli@hotmail.com>
2026-06-21 12:45:43 +0100

    Remove filename.txt

--- src/Location.cpp
+++
@@ -1,5 +0,0 @@
-#include "../headers/Location.hpp"
-
-Location::Location() : _autoindex(false), _redirectCode(0)
-{
-}
--- src/ServerConfig.cpp
+++
@@ -1,5 +0,0 @@
-#include "../headers/ServerConfig.hpp"
-
-ServerConfig::ServerConfig() : _port(0), _clientMaxBodySize(1048576)
-{
-}
